3f2a757521f542d8eae22b704d4191f71d952381
xrb16171 <xrb16171@uni.strath.ac.uk>
2019-02-22 15:27:17 +0000

    Preload articles: All articles are stored in localStorage.

--- GetArticle.js
+++ GetArticle.js
@@ -1,24 +1,27 @@
 var currentArticle = 0;
 
-function loadDoc(article) {
+function init(article) {
     currentArticle = article;
 
-    // Show the next button if there is another article
-    if(currentArticle === 5){
-        document.getElementsByTagName('footer').item(0).innerHTML =
-            '<button ><a href="rank.html">Rank articles</a></button>';
-    }else if(currentArticle < 5 && currentArticle > 0){
-        document.getElementsByTagName('footer').item(0).innerHTML =
-            '<button onclick="nextArticle()">Next article</button>';
-    }else{
-        console.log("The current article number exceed the boundaries! currentArticle: " + currentArticle);
+    //Load all of the articles into the local storage
+    if (currentArticle === 1) {
+        var i;
+        for (i = 1; i <= 5; i++)
+            saveArticles(i);
     }
 
-    // Set the title
-    document.title = "BBC Coding Test | Article " + currentArticle;
+    loadArticle(currentArticle);
 
+}
+
+function nextArticle() {
+    currentArticle++;
+    loadArticle(currentArticle);
+}
+
+function saveArticles(articleNum) {
     const xhr = new XMLHttpRequest();
-    xhr.open('get', 'articles/article-'+currentArticle+'.json');
+    xhr.open('get', 'articles/article-' + articleNum + '.json');
 
     // Track the state changes of the request.
     xhr.onreadystatechange = function () {
@@ -28,9 +31,7 @@ function loadDoc(article) {
                 // xhr.responseText is the content of the .json file
                 // Also.... why are all the articles in latin???
                 // Emptying the html file first just in case...
-
-                document.getElementById('article').innerHTML = "";
-
+                var articleData = '';
                 const jsonData = JSON.parse(xhr.responseText).body;
                 let x;
                 for (x = 0; x < jsonData.length; x++) {
@@ -38,14 +39,12 @@ function loadDoc(article) {
                     switch (type) {
                         case 'heading' :
                             // Set the headline of the article
-                            document.getElementById('article').innerHTML = '<h1>'+jsonData[x].model.text+'</h1>';
+                            articleData = articleData.concat('<h1>' + jsonData[x].model.text + '</h1>');
                             break;
 
                         case 'paragraph':
                             // Add each paragraph to the paragraphs div
-                            var newPara = document.getElementById('article').innerHTML;
-                            newPara = newPara.concat('<p>' + jsonData[x].model.text + '</p>');
-                            document.getElementById('article').innerHTML = newPara;
+                            articleData = articleData.concat('<p>' + jsonData[x].model.text + '</p>');
                             break;
 
                         case 'image':
@@ -55,9 +54,7 @@ function loadDoc(article) {
                             var width = jsonData[x].model.width;
 
                             // Add each of the parameters from above to an image tag in the HTML div 'images'
-                            var newImage = document.getElementById('article').innerHTML;
-                            newImage = newImage.concat('<img src="' + src + '" alt="' + alt + '" width="' + width + '" height="' + height + '">');
-                            document.getElementById('article').innerHTML = newImage;
+                            articleData = articleData.concat('<img src="' + src + '" alt="' + alt + '" width="' + width + '" height="' + height + '">');
                             break;
                         case 'list':
                             // Get the population of the list
@@ -70,17 +67,23 @@ function loadDoc(article) {
                             // Add it to the html after checking if it is ordered or unordered.
                             if (jsonData[x].model.type = 'unordered') {
                                 let list = '<ul>' + points + '</ul>';
-                                document.getElementById('article').innerHTML = document.getElementById('article').innerHTML + list;
-                            }else if(jsonData[x].model.type = 'ordered'){
+                                articleData = articleData.concat(list);
+                            } else if (jsonData[x].model.type = 'ordered') {
                                 let list = '<ol>' + points + '</ol>';
-                                document.getElementById('article').innerHTML = document.getElementById('article').innerHTML + list;
+                                articleData = articleData.concat(list);
                             }
                             break;
                         default:
                             console.log("Unrecognised type in article: " + type);
                     }
+
                 }
 
+
+
+
+                //Store the article data
+                localStorage.setItem(articleNum, articleData);
             } else {
                 alert('Error: ' + xhr.status); // Something went wrong trying to get the file if this happens!
             }
@@ -89,7 +92,20 @@ function loadDoc(article) {
     xhr.send(null); // Send the request
 }
 
-function nextArticle(){
-    // Load the next article
-    loadDoc(currentArticle+1);
+function loadArticle(aNum) {
+    document.title = "BBC Coding Test | Article " + aNum;
+    document.getElementById('article').innerHTML = localStorage.getItem(aNum);
+
+    // Add footer to the html
+    // Show the next button if there is another article
+    console.log('CA: '+currentArticle);
+    if (currentArticle === 5) {
+        document.getElementsByTagName('footer').item(0).innerHTML =
+            '<button ><a href="rank.html">Rank articles</a></button>';
+    } else if (currentArticle < 5 && currentArticle > 0) {
+        document.getElementsByTagName('footer').item(0).innerHTML =
+            '<button onclick="nextArticle()">Next article</button>';
+    } else {
+        console.log("The current article number exceed the boundaries! currentArticle: " + currentArticle);
+    }
 }
